8e0172e9235397ff2098b324a24c6a01026106b6
sebastian-sierrainteractive <sebastian@sierrainteractive.com>
2023-10-19 17:13:09 -0500

    feat: Access env vars through config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PresentationModule } from '@presentation/presentation.module';
 
 @Module({
-  imports: [PresentationModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: 'dev.env' }),
+    PresentationModule,
+  ],
 })
 export class AppModule {}
--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CallerId, CallerIdSchema } from './models/caller-id.model';
 import { CallerIdService } from './services/caller-id.service';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
     MongooseModule.forFeature([
       { name: CallerId.name, schema: CallerIdSchema },
     ]),
